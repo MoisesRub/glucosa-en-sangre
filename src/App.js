b654add940ede964ac22fff1d93506374233c5b8
Moises Rubio <moises.rubio97@hotmail.com>
2023-11-08 00:41:58 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import { Form, Input, InputNumber, Button } from 'antd';
+import 'antd/dist/reset.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    level: null,
+  });
+  const [apiResponse, setApiResponse] = useState(null);
+
+  const formDataWithCommand = {
+    ...formData,
+    command: 'read'
+  };
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await fetch('https://glucosasangre.000webhostapp.com/glucosa_api.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formDataWithCommand),
+      });
+  
+      //const data = await response.json();
+      const data = await response.json();
+
+      setApiResponse(data);
+  
+      // Handle the response data here
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Form>
+          <Form.Item>
+          <Input
+  placeholder='Nombre del paciente'
+  name='name'
+  value={formData.name}>
+  </Input>
+
+</Form.Item>
+          <Form.Item>
+          <InputNumber
+
+  placeholder="Nivel de glucosa en sangre"
+  name='level'
+  value={formData.level}
+  />
+
+          </Form.Item>
+          <Form.Item>
+          <Button type="primary" onClick={handleSubmit}>Registrar</Button>
+          </Form.Item>
+        </Form>
+
+
+        <div>
+     
+      </div>
+      {apiResponse && (
+  <div>
+    <h3>API Response:</h3>
+    <pre>{JSON.stringify(apiResponse, null, 2)}</pre>
+  </div>
+)}
+
       </header>
     </div>
+    
   );
 }
 
