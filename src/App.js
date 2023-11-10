d67b994822748eff527a6f77ceec2b2181dc5925
Moises Rubio <moises.rubio97@hotmail.com>
2023-11-10 11:26:52 -0500

    Modifique el app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import { Form, Input, InputNumber, Button } from 'antd';
-import 'antd/dist/reset.css';
 import React, { useState } from 'react';
+import { Form, Input, InputNumber, Button } from 'antd';
 
 
 function App() {
@@ -12,77 +9,72 @@ function App() {
   });
   const [apiResponse, setApiResponse] = useState(null);
 
-  const formDataWithCommand = {
-    ...formData,
-    command: 'read'
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       const response = await fetch('https://glucosasangre.000webhostapp.com/glucosa_api.php', {
         method: 'POST',
+         mode:'cors'
+        ,
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formDataWithCommand),
+        body: JSON.stringify({
+          ...formData,
+          command: 'read'
+        }),
       });
-  
-      //const data = await response.json();
-      const data = await response.json();
 
+      const data = await response.json();
       setApiResponse(data);
-  
-      // Handle the response data here
+
       console.log(data);
     } catch (error) {
       console.error('Error:', error);
     }
   };
-  
+
   return (
-    
     <div className="App">
       <header className="App-header">
-
         <Form>
           <Form.Item>
-          <Input
-  placeholder='Nombre del paciente'
-  name='name'
-  value={formData.name}>
-  </Input>
-
-</Form.Item>
-          <Form.Item>
-          <InputNumber
-
-  placeholder="Nivel de glucosa en sangre"
-  name='level'
-  value={formData.level}
-  />
-
+            <Input
+              placeholder='Nombre del paciente'
+              name='name'
+              value={formData.name}
+              onChange={handleChange}
+            />
           </Form.Item>
           <Form.Item>
-          <Button type="primary" onClick={handleSubmit}>Registrar</Button>
+            <InputNumber
+              placeholder="Nivel de glucosa en sangre"
+              name='level'
+              value={formData.level}
+              onChange={(value) => setFormData({ ...formData, level: value })}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" onClick={handleSubmit}>
+              Registrar
+            </Button>
           </Form.Item>
         </Form>
 
-
-        <div>
-     
-      </div>
-      {apiResponse && (
-  <div>
-    <h3>API Response:</h3>
-    <pre>{JSON.stringify(apiResponse, null, 2)}</pre>
-  </div>
-)}
-
+        {apiResponse && (
+          <div>
+            <h3>API Response:</h3>
+            <pre>{JSON.stringify(apiResponse, null, 2)}</pre>
+          </div>
+        )}
       </header>
     </div>
-    
   );
 }
 
