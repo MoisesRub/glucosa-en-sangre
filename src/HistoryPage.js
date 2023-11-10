3ccb62d0ab0324b31ab9a894dc81ee0f8b05a5df
Moises Rubio <moises.rubio97@hotmail.com>
2023-11-10 16:54:39 -0500

    Ya está el filtro, pero no logramos hacer que cambie la grafica, tambien la grafica está al reves

--- src/HistoryPage.js
+++ src/HistoryPage.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Input, InputNumber, Button, Table } from 'antd';
+import { Input, Button, Table } from 'antd';
 import 'antd/dist/reset.css';
 import './App.css';
 import Chart from 'chart.js/auto'; // Import Chart.js
 
-function HistoryPage() {  
+import { SearchOutlined } from '@ant-design/icons';
+
+function HistoryPage() {
   const [apiResponse, setApiResponse] = useState(null);
+  const [filteredData, setFilteredData] = useState(null);
+  const [chartInstance, setChartInstance] = useState(null);
 
   const columns = [
     {
@@ -13,12 +17,48 @@ function HistoryPage() {
       dataIndex: 'FECHA',
       key: 'FECHA',
       sorter: (a, b) => new Date(b.FECHA) - new Date(a.FECHA), // Sort by date in descending order
-     
     },
     {
       title: 'Nombre',
       dataIndex: 'NOMBRE',
       key: 'NOMBRE',
+      filters: [], // Initialize empty array for filters
+      onFilter: (value, record) => record.NOMBRE.includes(value), // Filter function
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 8 }}>
+          <Input
+            placeholder="Buscar nombre"
+            value={selectedKeys[0]}
+            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => confirm()}
+            style={{ width: 188, marginBottom: 8, display: 'block' }}
+          />
+          <Button
+            type="primary"
+            onClick={() => confirm()}
+            size="small"
+            style={{ width: 90, marginRight: 8 }}
+          >
+            Buscar
+          </Button>
+          <Button onClick={() => clearFilters()} size="small" style={{ width: 90 }}>
+            Reiniciar
+          </Button>
+        </div>
+      ),
+      filterIcon: (filtered) => (
+        <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+      ),
+      onFilterDropdownOpenChange: (visible) => {
+        if (visible) {
+          // Pre-fetch distinct values for the filter dropdown
+          const distinctNames = Array.from(new Set(apiResponse.map((record) => record.NOMBRE)));
+          columns.find((col) => col.key === 'NOMBRE').filters = distinctNames.map((name) => ({
+            text: name,
+            value: name,
+          }));
+        }
+      },
     },
     {
       title: 'Nivel',
@@ -32,28 +72,47 @@ function HistoryPage() {
     },
   ];
 
-
-
-  // Use useEffect to update the chart when the API response changes
+  // Use useEffect to fetch data when the component mounts
   useEffect(() => {
     async function fetchData() {
-        try {
-          const url = 'https://glucosasangre.000webhostapp.com/glucosa_api.php';
-          const response = await fetch(`${url}?command=get`);
-          const data = await response.json();
-  
-          // Sort the data by date in descending order
-          const sortedData = [...data].sort((a, b) => new Date(b.FECHA) - new Date(a.FECHA));
-  
-          // Extract NIVEL data for the histogram
-          const nivelData = sortedData.map((item) => parseFloat(item.NIVEL));
-  
-          // Create a histogram chart using Chart.js
-          const ctx = document.getElementById('histogramChart');
-      new Chart(ctx, {
+      try {
+        const url = 'https://glucosasangre.000webhostapp.com/glucosa_api.php';
+        const response = await fetch(`${url}?command=get`);
+        const data = await response.json();
+
+        // Set the API response in the state
+        setApiResponse(data);
+
+        // Sort the data by date in descending order
+        const sortedData = [...data].sort((a, b) => new Date(b.FECHA) - new Date(a.FECHA));
+
+        // Set the sorted data in the state
+        setFilteredData(sortedData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  // Use useEffect to update the chart when the filtered data changes
+  useEffect(() => {
+    if (filteredData) {
+      // Extract NIVEL data for the histogram
+      const nivelData = filteredData.map((item) => parseFloat(item.NIVEL));
+
+            // Destroy the previous chart if it exists
+            if (chartInstance) {
+                chartInstance.destroy();
+              }
+
+      // Create a histogram chart using Chart.js
+      const ctx = document.getElementById('histogramChart');
+      const newChartInstance = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: apiResponse.map((item) => item.FECHA),
+          labels: filteredData.map((item) => item.FECHA),
           datasets: [
             {
               label: 'Nivel de Glucosa',
@@ -83,15 +142,9 @@ function HistoryPage() {
           },
         },
       });
-        // Set the sorted data in the state
-        setApiResponse(sortedData);
-      } catch (error) {
-        console.error('Error:', error);
-      }
+      setChartInstance(newChartInstance);
     }
-
-    fetchData();
-  }, []);
+  }, [filteredData]);
   return (
     <div className="App">
       <header className="App-header">
